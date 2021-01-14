0fb10dd39ca1e717f94ce8f15f8fbaa6e2e41157
Conqr <34463817+conqr2@users.noreply.github.com>
2021-01-14 12:55:25 -0800

    Create auth.cs

---
+++ auth.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace AuthenticationForYallMateys
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("AUTHENTICATION FOR Y'ALL MATEYS");
+      Console.WriteLine("The ship is getting ready to sail.  ");
+      Console.WriteLine("The ship is getting ready to sail.. ");
+      Console.WriteLine("The ship is getting ready to sail...");
+      Console.WriteLine("The ship is getting ready to sail.. ");
+      Console.WriteLine("The ship is getting ready to sail.  ");
+      Console.WriteLine("⛵️🌊🌊🌊🌊🌊🌊🌊🌊");
+      Console.WriteLine("🌊⛵️🌊🌊🌊🌊🌊🌊🌊");
+      Console.WriteLine("🌊🌊⛵️🌊🌊🌊🌊🌊🌊");
+      Console.WriteLine("🌊🌊🌊⛵️🌊🌊🌊🌊🌊");
+      Console.WriteLine("🌊🌊🌊🌊⛵️🌊🌊🌊🌊");
+      Console.WriteLine("🌊🌊🌊🌊🌊⛵️🌊🌊🌊");
+      Console.WriteLine("🌊🌊🌊🌊🌊🌊⛵️🌊🌊");
+      Console.WriteLine("🌊🌊🌊🌊🌊🌊🌊⛵️🌊");
+      Console.WriteLine("🌊🌊🌊🌊🌊🌊🌊🌊⛵️");
+      Console.WriteLine("🌊🌊🌊🌊🌊🌊🌊🌊⛵️⚓️");
+      Console.WriteLine("Authenticating.   ");
+      Console.WriteLine("Authenticating..  ");
+      Console.WriteLine("Authenticating... ");
+      Console.WriteLine("Authenticating....");
+      Console.WriteLine("Authenticating... ");
+      Console.WriteLine("Authenticating..  ");
+      Console.WriteLine("Authenticating.   ");
+      Console.WriteLine("Authenticating..  ");
+      Console.WriteLine("Authenticating... ");
+      Console.WriteLine("Authenticating....");
+      Console.WriteLine("Authenticating... ");
+      Console.WriteLine("Authenticating..  ");
+      Console.WriteLine("Authenticating.   ");
+      Console.WriteLine("Authentication Complete!");
+      Console.WriteLine("Please visit: https://dis.gd/threads");
+    }
+  }
+}
